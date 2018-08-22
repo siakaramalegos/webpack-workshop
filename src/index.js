b39181c8a7b0c105c70c1a8393a4d2782958153a
Sia <siakaramalegos@gmail.com>
2018-08-22 11:11:19 -0500

    Add index.html and basic script

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function heading() {
+  let element = document.createElement('h1');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.capitalize('hello webpack!');
+
+  return element;
+}
+
+document.body.appendChild(heading());
